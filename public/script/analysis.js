00243483d012cfe09639b0bac7dca384d5f72ea0
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-07-24 18:46:38 +0700

    Make editor main page

---
+++ public/script/analysis.js
@@ -0,0 +1,11 @@
+import { createApp } from '../lib/vue.esm-browser.js';
+
+// Root component
+let component = {};
+
+// Setup vue
+const app = createApp(component);
+app.config.errorHandler = (err) => {
+    console.log(err);
+};
+app.mount('#app');
--- public/script/index.js
+++ public/script/index.js
@@ -1,7 +1,22 @@
 import { createApp } from '../lib/vue.esm-browser.js';
+import Common from './common.js';
 
 // Root component
-let component = {};
+let component = {
+    data() {
+        return {
+            board: Common.data.board,
+            size: Common.data.size,
+        }
+    },
+    methods: {
+        getPosition: Common.methods.getPosition,
+        getRank: Common.methods.getRank,
+        getFileLetter: Common.methods.getFileLetter,
+        isDarkSquared: Common.methods.isDarkSquared,
+        isPiece: Common.methods.isPiece,
+    }
+};
 
 // Setup vue
 const app = createApp(component);
