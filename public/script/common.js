96898f902f529fe134689199ddf1bcd0b0dc58a0
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-07-24 20:48:30 +0700

    Replace image on drop, remove if drop outside

--- public/script/common.js
+++ public/script/common.js
@@ -20,6 +20,7 @@ export default {
         },
         handler: {
             onDragStart(ev) {
+                // Set data to dragged item id
                 ev.dataTransfer.setData("text/plain", ev.target.id);
                 ev.dataTransfer.effectAllowed = "move";
             },
@@ -27,9 +28,18 @@ export default {
                 ev.dataTransfer.dropEffect = "move";
             },
             onDrop(ev) {
+                // Get dragged item id
                 const id = ev.dataTransfer.getData("text/plain");
-                ev.target.appendChild(document.getElementById(id));
+                let el = document.getElementById(id);
+                
+                // If drop zone id not "bin" move item and update image src, else remove
+                if(ev.target.id !== "bin") {
+                    ev.target.appendChild(document.getElementById(id));
+                    ev.target.src = el.src;
+                } else {
+                    el.parentNode.removeChild(el);
+                }
             }
-        }  
+        }
     },
 };
