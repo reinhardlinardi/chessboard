f9c81d43815dc93e4fb240f90089a07d607dcaa3
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-07-26 15:20:50 +0700

    Revert position to 2d string array to fix drag and drop

--- src/script/position.ts
+++ src/script/position.ts
@@ -1,6 +1,6 @@
 import * as Piece from './piece.js';
 
-const none: string = Piece.None;
+const na: string = Piece.None;
 const wp: string = Piece.WhitePawn.letter;
 const bp: string = Piece.BlackPawn.letter;
 const wn: string = Piece.WhiteKnight.letter;
@@ -14,32 +14,32 @@ const bq: string = Piece.BlackQueen.letter;
 const wk: string = Piece.WhiteKing.letter;
 const bk: string = Piece.BlackKing.letter;
 
-export const Empty: string[] = [
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
+export const Empty: string[][] = [
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
 ];
 
-export const Default: string[] = [
-    [wr, wn, wb, wq, wk, wb, wn, wr].join(""),
-    wp.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    none.repeat(8),
-    bp.repeat(8),
-    [br, bn, bb, bq, bk, bb, bn, br].join(""),
+export const Default: string[][] = [
+    [wr, wn, wb, wq, wk, wb, wn, wr],
+    [wp, wp, wp, wp, wp, wp, wp, wp],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [na, na, na, na, na, na, na, na],
+    [bp, bp, bp, bp, bp, bp, bp, bp],
+    [br, bn, bb, bq, bk, bb, bn, br],
 ]
 
-export function copy(position: string[]): string[] {
-    let copy: string[] = new Array(8);
+export function copy(position: string[][]): string[][] {
+    let copy: string[][] = new Array(8);
     for(let rank = 0; rank < 8; rank++) {
-        copy[rank] = position[rank];
+        copy[rank] = [...position[rank]];
     }
     
     return copy;
