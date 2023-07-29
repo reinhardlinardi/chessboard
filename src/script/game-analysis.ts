3f67420763e4fdcf53c964fc857dd312b6c29652
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-07-29 19:17:32 +0700

    Rename game to game analysis

---
+++ src/script/game-analysis.ts
@@ -0,0 +1,22 @@
+// import * as Setup from './setup.js';
+// import * as Position from './position.js';
+
+// export class GameAnalysis {
+//     private position: string[][];
+
+//     // private started: boolean;
+//     // private ended: boolean;
+
+
+//     public constructor() {
+//         this.position = Setup.getDefaultSetup();
+//         // this.started = false;
+//         // this.ended = false;
+//     }
+
+//     // public getPosition(): string[][] {
+//     //     return Position.copy(this.position);
+//     // }
+
+//     // TODO: When attempting to start game, validate position first
+// }
