d9bc68ca111b609f00768beef38321ffea066491
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-08-01 22:32:38 +0700

    Add missing return type part 2

--- src/script/rank.ts
+++ src/script/rank.ts
@@ -1,6 +1,6 @@
 import * as Color from './color.js';
 
 // Return absolute rank from relative rank
-export function nthRank(n: number, color: string) {
+export function nthRank(n: number, color: string): number {
     return color == Color.White? n : 9-n; 
 }
