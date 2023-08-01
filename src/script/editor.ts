152879dc7b34d99303da3f36e402628368534f1d
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-08-01 11:17:37 +0700

    Fix editor Position type

--- src/script/editor.ts
+++ src/script/editor.ts
@@ -1,11 +1,17 @@
 import * as Castle from './castle.js';
-import { Board, getPiece, setPiece } from './board.js';
+import * as Piece from './piece.js';
+import { Position, get, set } from './position.js';
 
-export function hasCastlePosition(board: Board, type: string) {
+
+const P = Piece.WhitePawn.letter;
+const p = Piece.BlackPawn.letter;
+
+
+export function hasCastlePosition(pos: Position, type: string) {
     const castle = Castle.get(type);
     const king = castle.king;
     const rook = castle.rook;
 
-    return getPiece(board, king.square.rank, king.square.file) === king.piece &&
-        getPiece(board, rook.square.rank, rook.square.file) === rook.piece;
+    return get(pos, king.square.rank, king.square.file) === king.piece &&
+        get(pos, rook.square.rank, rook.square.file) === rook.piece;
 }
