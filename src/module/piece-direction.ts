7a32ebf3a17500a56b2374598bd7e2ec2b0b8e1d
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-08-19 22:16:53 +0700

    Rename import

--- src/module/piece-direction.ts
+++ src/module/piece-direction.ts
@@ -1,14 +1,14 @@
-import * as D from './direction.js';
+import * as Drc from './direction.js';
 
 
-export const WhitePawnAdvance: readonly D.Direction[] = Object.freeze([D.Black]);
-export const WhitePawnCapture: readonly D.Direction[] = Object.freeze(D.BlackDiagonal);
+export const WhitePawnAdvance: readonly Drc.Direction[] = Object.freeze([Drc.Black]);
+export const WhitePawnCapture: readonly Drc.Direction[] = Object.freeze(Drc.BlackDiagonal);
 
-export const BlackPawnAdvance: readonly D.Direction[] = Object.freeze([D.White]);
-export const BlackPawnCapture: readonly D.Direction[] = Object.freeze(D.WhiteDiagonal);
+export const BlackPawnAdvance: readonly Drc.Direction[] = Object.freeze([Drc.White]);
+export const BlackPawnCapture: readonly Drc.Direction[] = Object.freeze(Drc.WhiteDiagonal);
 
-export const Knight: readonly D.Direction[] = Object.freeze([21, 19, 12, 8, -8, -12, -19, -21]);
-export const Bishop: readonly D.Direction[] = Object.freeze(D.Diagonal);
-export const Rook: readonly D.Direction[] = Object.freeze([...D.Vertical, ...D.Horizontal]);
-export const Queen: readonly D.Direction[] = Object.freeze(D.All);
-export const King: readonly D.Direction[] = Object.freeze(D.All);
+export const Knight: readonly Drc.Direction[] = Object.freeze([21, 19, 12, 8, -8, -12, -19, -21]);
+export const Bishop: readonly Drc.Direction[] = Object.freeze(Drc.Diagonal);
+export const Rook: readonly Drc.Direction[] = Object.freeze([...Drc.Vertical, ...Drc.Horizontal]);
+export const Queen: readonly Drc.Direction[] = Object.freeze(Drc.All);
+export const King: readonly Drc.Direction[] = Object.freeze(Drc.All);
