7f345b872f68e60ece8781fb201ff478a19945e5
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-08-20 14:31:00 +0700

    Tidy up server.js

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ const port = 8080;
 
 // Cache favicon and SVG for a long time
 app.use((req, res, next) => {
-    if(req.url.match(/\.(?:ico|svg)/)) res.setHeader('Cache-Control', 86400);
+    if(req.url.match(/\.(?:ico|svg)/)) {
+        res.setHeader('Cache-Control', 86400);
+    }
     next();
 });
 
-app.use(express.static("public", {extensions: ['html']}));
+app.use(express.static("public", {
+    extensions: ['html'],
+}));
 
-app.listen(port, () => { console.log(`Listening on :${port}`) });
+app.listen(port, () => {
+    console.log(`Listening on :${port}`);
+});
