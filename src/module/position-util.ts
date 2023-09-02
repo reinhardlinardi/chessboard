2ca4506049a93885698800a7bcc8deee96d7dc36
Reinhard Linardi <reinhardlinardi@gmail.com>
2023-09-02 17:42:32 +0700

    Position util tidy up import

--- src/module/position-util.ts
+++ src/module/position-util.ts
@@ -1,16 +1,14 @@
-import * as Piece from './piece.js';
 import * as Loc from './location.js';
-import * as File from './file.js';
 import * as EnPassant from './en-passant.js';
+import * as Pieces from './pieces.js';
 import { Size as size } from './size.js';
 import { Position, get, getByLoc } from './position.js';
 import { TypeKing, TypePawn } from './piece-type.js';
-import { get as getPiece, getBy as pieceBy } from './pieces.js';
-import { Color, getList as getColors, opponentOf } from './color.js';
+import { Color, opponentOf, getList as getColors } from './color.js';
 
 
 type Location = Loc.Location;
-type EnPassantLoc = {[c: Color]: Location[]};
+type EnPassantPawns = {[c: Color]: Location[]};
 
 
 export function outOfBound(loc: Location): boolean {
@@ -21,7 +19,7 @@ export function outOfBound(loc: Location): boolean {
 }
 
 export function getKingLoc(pos: Position, color: Color): Location {
-    const king = pieceBy(color, TypeKing).letter;
+    const king = Pieces.getBy(color, TypeKing).letter;
 
     for(let rank = 1; rank <= size; rank++) {
         for(let file = 1; file <= size; file++) {
@@ -33,39 +31,24 @@ export function getKingLoc(pos: Position, color: Color): Location {
     return Loc.None;
 }
 
-export function getEnPassantPawns(file: number, pos: Position, player: Color): EnPassantLoc {
-    let loc: EnPassantLoc = {};
-    const opponent = opponentOf(player);
-
-    const pawn: {[c: Color]: string} = {};
+export function getEnPassantPawns(file: number, pos: Position, player: Color): EnPassantPawns {
+    let pawns: EnPassantPawns = {};
+    
     const colors = getColors();
-
     for(const color of colors) {
-        pawn[color] = pieceBy(color, TypePawn).letter;
-        loc[color] = [];
+        pawns[color] = [];
     }
+    
+    const opponent = opponentOf(player);
+    const playerPawn = Pieces.getBy(player, TypePawn).letter;
+    const opponentPawn = Pieces.getBy(opponent, TypePawn).letter;
 
-    const opponentPawn = EnPassant.opponentPawnLoc(file, player);
-    if(getByLoc(pos, opponentPawn) === pawn[opponent]) loc[opponent].push(opponentPawn);
+    const opponentLoc = EnPassant.opponentPawnLoc(file, player);
+    if(getByLoc(pos, opponentLoc) === opponentPawn) pawns[opponent].push(opponentLoc);
 
-    const playerPawns = EnPassant.playerPawnsLoc(file, player);
-    for(const playerPawn of playerPawns) {
-        if(getByLoc(pos, playerPawn) === pawn[player]) loc[player].push(playerPawn);
+    const playerLoc = EnPassant.playerPawnsLoc(file, player);
+    for(const loc of playerLoc) {
+        if(getByLoc(pos, loc) === playerPawn) pawns[player].push(loc);
     }
-    return loc;
-}
-
-export function isRookPawn(loc: Location, pos: Position): boolean {
-    if(!isPawn(loc, pos)) return false;
-
-    const file = Loc.file(loc);
-    return file === File.a || file === File.h;
-}
-
-function isPawn(loc: Location, pos: Position): boolean {
-    const subject = getByLoc(pos, loc);
-    if(subject === Piece.None) return false;
-
-    const piece = getPiece(subject);
-    return piece.type === TypePawn;
+    return pawns;
 }
